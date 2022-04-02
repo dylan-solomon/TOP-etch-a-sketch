3a5d7cfbb84f504057fe0ba599c9e071cdbb6dc9
Dylan Solomon <dylansolomon413@gmail.com>
2022-04-02 16:43:19 -0700

    Add grid style to layout

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const box = document.querySelector('.board-container');
-
 const resetBtn = document.querySelector('.reset');
 const gridSize = document.getElementById('box-size');
 const size = document.querySelector('.size');
-
-
 
 /* Grid Size Indicator */
 size.textContent = `${gridSize.value} x ${gridSize.value}`;
@@ -15,16 +12,20 @@ gridSize.addEventListener('mouseup', () => {
 });
 
 
-/* Grid Creation & Reset*/
+/* Grid Creation*/
 function createGrid() {
-    for (let i = 0; i <= (gridSize.value * gridSize.value); i++) {
+    for (let i = 1; i <= (gridSize.value * gridSize.value); i++) {
         const gridItem = document.createElement('div');
         gridItem.className=`grid-item ${i}`;
+        gridItem.textContent=" ";
+        gridItem.style.border = "1px solid black";
         box.appendChild(gridItem);
     };
     size.textContent = `${gridSize.value} x ${gridSize.value}`;
+    box.style.gridTemplateColumns = `repeat(${gridSize.value}, 1fr)`;
 };
 
+/* Grid Reset*/
 function resetGrid() {
     while (box.lastChild) {
         box.removeChild(box.lastChild);
