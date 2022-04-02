77e8dedd332e26f05db1e46078d7e28934e7f31d
Dylan Solomon <dylansolomon413@gmail.com>
2022-04-02 15:56:44 -0700

    Allow grid reset

--- app.js
+++ app.js
@@ -7,16 +7,27 @@ const size = document.querySelector('.size');
 /* Grid Size Indicator */
 size.textContent = `${gridSize.value} x ${gridSize.value}`;
 
-gridSize.addEventListener('mouseup', () => {size.textContent = `${gridSize.value} x ${gridSize.value}`;});
+gridSize.addEventListener('mouseup', () => {
+    size.textContent = `${gridSize.value} x ${gridSize.value}`;
+    resetGrid();
+    createGrid();
+});
 
 
-
+/* Grid Creation & Reset*/
 function createGrid() {
     for (let i = 0; i <= (gridSize.value * gridSize.value); i++) {
         const gridItem = document.createElement('div');
         gridItem.className=`grid-item ${i}`;
         box.appendChild(gridItem);
-    }
-}
+    };
+};
+
+function resetGrid() {
+    while (box.lastChild) {
+        box.removeChild(box.lastChild);
+    };
+};
 
 createGrid()
+
